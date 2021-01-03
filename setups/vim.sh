106ece05a5b18de8f9faade6196f5c64675ae222
Emrah Urhan <1366577+raxetul@users.noreply.github.com>
2021-01-04 01:36:30 +0300

    Locating director fix

--- setups/vim.sh
+++ setups/vim.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+DIR=$(dirname "$0")/..
 
 
-test -f "$HOME/.vimrc"  &&  echo "$HOME/.vimrc exists" ||  ln -s "$DIR/conf/vim.rc" "$HOME/.vimrc"
+# test -f "$HOME/.vimrc"  &&  echo "$HOME/.vimrc exists" ||  ln -s "$DIR/conf/vim.rc" "$HOME/.vimrc"
 
-# test ! -d "~/.vim/bundle/Vundle.vim" && git clone "https://github.com/VundleVim/Vundle.vim.git" "~/.vim/bundle/Vundle.vim"
+# # test ! -d "~/.vim/bundle/Vundle.vim" && git clone "https://github.com/VundleVim/Vundle.vim.git" "~/.vim/bundle/Vundle.vim"
 
-test -d "~/.vim/pack/onedark/" && cd ~/.vim/pack/onedark/; echo "Vim onedark theme is found, trying to update..."; git pull --rebase || git clone https://github.com/joshdick/onedark.vim.git ~/.vim/pack/onedark
-echo "Vim is set up..."
+# test -d "~/.vim/pack/onedark/" && cd ~/.vim/pack/onedark/; echo "Vim onedark theme is found, trying to update..."; git pull --rebase || git clone https://github.com/joshdick/onedark.vim.git ~/.vim/pack/onedark
+# echo "Vim is set up..."
