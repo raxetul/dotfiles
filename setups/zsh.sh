6a2adae981164f3b361eb76a5891c1108ef56b60
Emrah URHAN <1366577+raxetul@users.noreply.github.com>
2022-06-26 23:40:34 +0300

    Merge of MacOS changes

--- setups/zsh.sh
+++ setups/zsh.sh
@@ -1,25 +1,31 @@
 #!/usr/bin/env zsh
+
 ROOTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )/.."
 ZSH_FILE=${HOME}/.zshrc
+echo E
+
+# [[ "$(command -v zsh)" ]] && zsh
 
 ZSH_BIN=`which zsh`
-echo $DISTRO
-if [[ "$DISTRO" != "Darwin" ]];
+
+if [ "$(sed 's#.*/##' <<<  $SHELL)" != "zsh" ]
 then
-    zsh
+    zsh # TODO: this has a problem, if shell changes, running this script will stop at this point
+    # This part may be irrelevant, shebang should guarantee the shell is being zsh
 fi
+
 AUTO_INSTALLED_USER_LINES_START="### AUTO ADDED USER LINES ############### START"
 AUTO_INSTALLED_USER_LINES_END="### AUTO ADDED USER LINES ############### END"
-
 # Activate oh-my-zsh plugins
-#PLUGINS=`perl -pe "s/^/\t/" ${ROOTDIR}/lists/oh-my-zsh-plugins.list`
-#perl -i -pe "BEGIN{undef $/;} s/^plugins=.*\)/plugins=(\n$PLUGINS\n)/smg" ~/.zshrc
+PLUGINS=`perl -pe "s/^/\t/" ${ROOTDIR}/lists/zplug-plugins.list`
+perl -i -pe "BEGIN{undef $/;} s/^plugins=.*\)/plugins=(\n$PLUGINS\n)/smg" ~/.zshrc
 
+echo EE
 [[ ! -d "${HOME}/.oh-my-zsh" ]] && sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 export ZPLUG_HOME=${ROOTDIR}/.installed/zplug
+echo "Zplug installation directory: $ZPLUG_HOME"
 [[ ! -d "${ZPLUG_HOME}" ]] && git clone https://github.com/zplug/zplug $ZPLUG_HOME
-
 
 SETTINGS="$AUTO_INSTALLED_USER_LINES_START
 # Don not modifiy this block, add your lines after END line.
@@ -33,7 +39,7 @@ do
     ELAPSED_TIME=\$((END_TIME - START_TIME))
     echo \"Activated \" \`basename \$SETTING_FILE\` \" in \" \$ELAPSED_TIME \" ms\"
 done
-
+fpath+=~/.zsh
 $AUTO_INSTALLED_USER_LINES_END"
 
 chsh -s ${ZSH_BIN} $USER
@@ -54,7 +60,10 @@ echo "ZPLUG_HOME is: ${ZPLUG_HOME}"
 
 source $ZPLUG_HOME/init.zsh
 
-
 source ${ROOTDIR}/lists/zplug-plugins.list
+
 zplug install 
 zplug load
+echo F
+
+[[ ! -d "${HOME}/.oh-my-zsh" ]]  && mkdir -p "${HOME}/.zsh" && flamegraph --completions zsh > "${HOME}/.zsh/.flamegraph-completion.zsh" && 
